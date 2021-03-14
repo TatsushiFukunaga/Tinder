2d7d1c4a7da063e09bf9fbd49398458588e7b516
tatsushi.fukunaga <tatsushi1789@gmail.com>
2021-03-15 00:35:35 +0900

    FireAuthに認証情報を保存

---
+++ Tinder/Views/Register/RegisterTitleLabel.swift
@@ -0,0 +1,23 @@
+//
+//  RegisterTitleLabel.swift
+//  Tinder
+//
+//  Created by Tatsushi Fukunaga on 2021/03/14.
+//
+
+import UIKit
+
+class RegisterTitleLabel: UILabel {
+    
+    init() {
+        super.init(frame: .zero)
+        self.text = "Tinder"
+        self.font = .boldSystemFont(ofSize: 80)
+        self.textColor = .white
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
